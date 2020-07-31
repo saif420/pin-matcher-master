d94c0f9e1feb8130acb5b66ff5f44f23263df3f1
saif420 <saifmahmud42069@gmail.com>
2020-07-31 07:33:14 +0600

    error

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 document.getElementById("left").style.display = 'none';
 
 // generate pin handler
+
 const  generateBtn = document.getElementById("generate-pin").addEventListener("click",function(){
     let randomNewNumber = Math.floor(1000 + Math.random() * 9000);
     let inputPin =  document.getElementById("generate-number").Value = randomNewNumber;
 })
 
 // funcation for input pin
+
 document.getElementById("generate-pin").Value;
 document.getElementById("screen-pin").Value;
 let result = document.getElementById("generate-pin");
 
 function button(num){
     let inputPin = result;
-    inputPin.Value +=num
+    inputPin.Value +=num;
 }
 
 // funcation  back 
+
 function back(){
     let screen = document.getElementById("screen-pin").Value;
     let back = screen.slice( 0, -1);
     document.getElementById("screen-pin").Value = back;
 }
 // funcation cancel 
-function cancel() {
+
+function cancel( ) {
     let screen = document.getElementById("screen-pin").Value;
     let cancel = ("");
     document.getElementById("screen-pin").Value = cancel;
@@ -31,3 +35,23 @@ function cancel() {
     document.getElementById("pin-wrong").style.display = 'none';
 
 }
+
+// funcation pin macthing
+
+let screen = document.getElementById("screen-pin").Value;
+let submitBtn=document.getElementById('submit-button');
+submitBtn.addEventListener("click",function(){
+    let randomValue = document.getElementById("generate-number").Value;
+    let totalResult = result.Value;
+    
+    let matched = document.getElementById("pin-matched");
+    let wrong = document.getElementById("pin-wrong");
+    
+    if (totalResult == randomValue) {
+        matched.style.display='block';
+    }
+    else{
+        matched.style.display='none';
+        wrong.style.display='block';
+    }
+})
